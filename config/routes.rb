0dabc00a2b8c45f6cd60c51fefc5b493bdc2c7d7
Shasha Yan <shashayan@gmail.com>
2021-06-20 13:04:11 -0500

    add link_to and RESTful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+  root 'sessions#welcome'
+  get '/' => 'sessions#welcome'
+
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   resources :courses
   resources :skills
   resources :users
